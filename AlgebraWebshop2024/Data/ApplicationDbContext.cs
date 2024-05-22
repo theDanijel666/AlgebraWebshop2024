293e11cde07cc6c67855184d416f237251bf9ae9
Danijel Pobi <121439317+theDanijel666@users.noreply.github.com>
2024-05-22 22:06:37 +0200

    Profile finished

--- AlgebraWebshop2024/Data/ApplicationDbContext.cs
+++ AlgebraWebshop2024/Data/ApplicationDbContext.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace AlgebraWebshop2024.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(100)]
+        public string? FirstName { get; set; }
+        [StringLength(100)]
+        public string? LastName { get; set; }
+        [StringLength(200)]
+        public string? Address { get; set; }
+
+        [StringLength(11,MinimumLength=11)]
+        public string? OIB {  get; set; }
+    }
+
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
---
+++ AlgebraWebshop2024/Data/Migrations/20240522185539_ExtendIdentityUser.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlgebraWebshop2024.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendIdentityUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OIB",
+                table: "AspNetUsers",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "OIB",
+                table: "AspNetUsers");
+        }
+    }
+}
